abe0821f8bcef47fd9204fcb33367039c69b1b79
upasi-shah <upasishah1032@gmail.com>
2026-08-08 18:50:24 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,7 +12,10 @@ accountCity = "Ahmedabad"
 /* 
 Prefer not to use var
 because of issue in block scope and functional scope
-
+- block scope means it has been declared using { } or we can say that in used in loops, conditional statments etc
+- if we are defining any value to the the variable it gives output as undefined
+- if have used below console.table because we don't want declared again and again console.log and each variable name
+ so using table we can get them in table format
 */
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(3
+    +
+     3) // code readability should be high 
+
+console.log("Upasi");
+
+let name ="upasi"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+/* !Important Question
+What re typeof null and undefined ??
+- typeof null is object and typeof undefined is undefined
+*/
