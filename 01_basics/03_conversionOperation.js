1fbabb8b75f38edc123679f4685b178bfaf27cf4
upasi-shah <upasishah1032@gmail.com>
2026-08-09 19:28:04 +0530

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "upasi";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// "33" => 33
+// "33abc" => NaN( Not a Number )
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "upasi" => true
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
